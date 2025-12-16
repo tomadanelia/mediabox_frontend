ea0ee1de6c80173a7f5028c70a71f6cd30ec2bf7
tomadanelia <tomadanelia16@gmail.com>
2025-12-17 00:34:06 +0400

    configured pro for shadcn

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,30 @@
 import { Button } from "@/components/ui/button"
+import Navbar from "./components/shadcn-studio/blocks/navbar-component-11/navbar-component-11"
+
+const navigationData = [
+  {
+    title: 'Home',
+    href: '#'
+  },
+  {
+    title: 'Dashboard',
+    href: '#'
+  },
+  {
+    title: 'Pricing',
+    href: '#'
+  },
+  {
+    title: 'Product Details',
+    href: '#'
+  }
+]
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
+      <Navbar navigationData={navigationData} />
     </div>
   )
 }
