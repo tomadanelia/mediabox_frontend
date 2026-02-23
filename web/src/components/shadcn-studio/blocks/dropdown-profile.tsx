47e49b0305ccb11f49cdb504bb55d4f95abd8bb8
Papuna Datunashvili <papunadatunashvili2@gmail.com>
2026-02-23 19:02:27 +0400

    commit desparate some changes about admin and profile pages

--- web/src/components/shadcn-studio/blocks/dropdown-profile.tsx
+++ web/src/components/shadcn-studio/blocks/dropdown-profile.tsx
@@ -4,13 +4,10 @@ import {
   UserIcon,
   SettingsIcon,
   CreditCardIcon,
-  UsersIcon,
-  SquarePenIcon,
-  CirclePlusIcon,
   LogOutIcon
 } from 'lucide-react'
 
-import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -35,14 +32,13 @@ const ProfileDropdown = ({ trigger, defaultOpen, align = 'end' }: Props) => {
         <DropdownMenuLabel className='flex items-center gap-4 px-4 py-2.5 font-normal'>
           <div className='relative'>
             <Avatar className='size-10'>
-              <AvatarImage src='https://cdn.shadcnstudio.com/ss-assets/avatar/avatar-1.png' alt='John Doe' />
-              <AvatarFallback>JD</AvatarFallback>
+              <AvatarFallback>TD</AvatarFallback>
             </Avatar>
             <span className='ring-card absolute right-0 bottom-0 block size-2 rounded-full bg-green-600 ring-2' />
           </div>
           <div className='flex flex-1 flex-col items-start'>
-            <span className='text-foreground text-lg font-semibold'>John Doe</span>
-            <span className='text-muted-foreground text-base'>john.doe@example.com</span>
+            <span className='text-foreground text-lg font-semibold'>Toma Danelia</span>
+            <span className='text-muted-foreground text-base'>tomadanelia16@gmail.com</span>
           </div>
         </DropdownMenuLabel>
 
@@ -51,32 +47,15 @@ const ProfileDropdown = ({ trigger, defaultOpen, align = 'end' }: Props) => {
         <DropdownMenuGroup>
           <DropdownMenuItem className='px-4 py-2.5 text-base'>
             <UserIcon className='text-foreground size-5' />
-            <span>My account</span>
+            <span>My Profile</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem className='px-4 py-2.5 text-base'>
+            <CreditCardIcon className='text-foreground size-5' />
+            <span>Subscription</span>
           </DropdownMenuItem>
           <DropdownMenuItem className='px-4 py-2.5 text-base'>
             <SettingsIcon className='text-foreground size-5' />
             <span>Settings</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem className='px-4 py-2.5 text-base'>
-            <CreditCardIcon className='text-foreground size-5' />
-            <span>Billing</span>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-
-        <DropdownMenuSeparator />
-
-        <DropdownMenuGroup>
-          <DropdownMenuItem className='px-4 py-2.5 text-base'>
-            <UsersIcon className='text-foreground size-5' />
-            <span>Manage team</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem className='px-4 py-2.5 text-base'>
-            <SquarePenIcon className='text-foreground size-5' />
-            <span>Customization</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem className='px-4 py-2.5 text-base'>
-            <CirclePlusIcon className='text-foreground size-5' />
-            <span>Add team account</span>
           </DropdownMenuItem>
         </DropdownMenuGroup>
 
