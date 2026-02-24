ac05d1d10713338d5b81bee147c63e811c02b468
Papuna Datunashvili <papunadatunashvili2@gmail.com>
2026-02-24 16:08:47 +0400

    removing proxy

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,21 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      '/proxy': {
-        target: 'http://159.89.20.100',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/proxy/, ''),
-      },
     },
   },
 })
