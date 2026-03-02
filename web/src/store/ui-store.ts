0af1aff12eb82bab6210b918100272e403f837c8
tomadanelia <tomadanelia16@gmail.com>
2026-03-02 16:35:02 +0400

    added persist middleware in uistore

--- web/src/store/ui-store.ts
+++ web/src/store/ui-store.ts
@@ -1,21 +1,26 @@
 import { create } from "zustand"
+import { persist } from "zustand/middleware"
 
 type Language = "En" | "Ge"
 
-type UIStore = {
+export type UIStore = {
   isDark: boolean
   language: Language
   toggleDarkMode: () => void
   setLanguage: (language: Language) => void
 }
 
-const useUIStore = create<UIStore>((set, get) => ({
-  isDark: true,
-  language: "Ge",
-  toggleDarkMode: () => set((state) => ({ isDark: !state.isDark })),
-  setLanguage: (language) => set({ language })
-}))
-
-export default useUIStore
-export type { Language, UIStore }
-
+const useUIStore = create<UIStore>()(
+  persist(
+    (set) => ({
+      isDark: true,
+      language: "Ge",
+      toggleDarkMode: () => set((state) => ({ isDark: !state.isDark })),
+      setLanguage: (language) => set({ language }),
+    }),
+    {
+      name: "ui-storage",
+    }
+  )
+)
+export default useUIStore;
